a6600d2a6e7d8ae8a3dad1f1d463b4cae2b5d864
Derick <derickchoskinson@gmail.com>
2022-09-07 22:34:18 -0500

    migrations

---
+++ prisma/migrations/20220908033324_init/migration.sql
@@ -0,0 +1,104 @@
+-- CreateEnum
+CREATE TYPE "Pronouns" AS ENUM ('THEY', 'HE', 'SHE');
+
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('USER', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "password" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'USER',
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "body" TEXT NOT NULL,
+    "postImg" TEXT NOT NULL,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Profile" (
+    "id" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "bio" TEXT NOT NULL,
+    "birthDay" TIMESTAMP(3) NOT NULL,
+    "currentLocation" TEXT NOT NULL,
+    "pronouns" "Pronouns" NOT NULL DEFAULT 'THEY',
+    "occupation" TEXT NOT NULL,
+    "profilePicture" TEXT NOT NULL DEFAULT 'https://remix-bucket.s3.us-east-2.amazonaws.com/Derick2022.jpeg',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+
+    CONSTRAINT "Profile_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_CategoryToPost" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_id_key" ON "User"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Post_id_key" ON "Post"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category_id_key" ON "Category"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category_name_key" ON "Category"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Profile_id_key" ON "Profile"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Profile_userId_key" ON "Profile"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_CategoryToPost_AB_unique" ON "_CategoryToPost"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_CategoryToPost_B_index" ON "_CategoryToPost"("B");
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Profile" ADD CONSTRAINT "Profile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_CategoryToPost" ADD CONSTRAINT "_CategoryToPost_A_fkey" FOREIGN KEY ("A") REFERENCES "Category"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_CategoryToPost" ADD CONSTRAINT "_CategoryToPost_B_fkey" FOREIGN KEY ("B") REFERENCES "Post"("id") ON DELETE CASCADE ON UPDATE CASCADE;
---
+++ prisma/seed.ts
@@ -0,0 +1,110 @@
+const { PrismaClient } = require('@prisma/client')
+const bcrypt = require('bcryptjs')
+
+const prisma = new PrismaClient()
+
+async function seed() {
+  const email = 'iderick@gmail.com'
+
+  // cleanup the existing database
+  await prisma.user.delete({ where: { email: email } }).catch(() => {
+    // no worries if it doesn't exist yet
+  })
+
+  const hashedPassword = await bcrypt.hash('1234567', 10)
+
+  const user = await prisma.user.create({
+    data: {
+      email,
+      password: hashedPassword,
+      role: 'ADMIN'
+    }
+  })
+
+  await prisma.profile.create({
+    data: {
+      user: { connect: { id: user.id } },
+      email: user.email,
+      firstName: 'Derick',
+      lastName: 'Hoskinson',
+			title:'Senior Clinical Scientist',
+      bio: 'I am a senior clinical geneticist and a newbie software developer',
+      currentLocation: 'Chicago',
+      birthDay: '1980-03-03T19:00:52Z',
+      occupation: 'scientist',
+      pronouns: 'HE',
+      profilePicture: 'https://blogphotosbucket.s3.us-east-2.amazonaws.com/profileimages/DerickFace.jpg'
+    }
+  })
+
+  const post1 = await prisma.post.create({
+    data: {
+      title: 'Creating my personal blog site',
+      body: `'1. Why didn’t i just use a blogging website like Wix or something like Astro.build?
+    1. Real reason - I’m stubborn
+    2. I tried Wix but didn’t enjoy the feel of the editor even when I turned on the more developer friendly settings
+    3. Astro is something that has been poking at my interests but I didn’t understand how I might actually use it in this project
+        1. More on this later though because one of the Astro templates helped me
+            1. Go through the process of learning the very basics of Astro and along the way my brain was processing how I might be able to use the exposure (seeing a site, copying that site for practice) to inform my coding process for my blog
+2. What are my main wants for V1 of the personal blog?
+    1. Personal Login, a feeds page, a drafts page, a profile/about page'`,
+      postImg: 'https://blogphotosbucket.s3.us-east-2.amazonaws.com/postimages/post_one_prisma_schema.png',
+      published: true,
+      userId: user.id,
+      categories: {
+        create: [
+          { name: 'Coding' },
+          {
+            name: 'Typescript'
+          }
+        ]
+      }
+    }
+  })
+
+const post2 = await prisma.post.create({
+    data: {
+      title: 'My first Typescript App',
+      body: 'How I went about building my first working typescript app',
+      postImg: 'https://blogphotosbucket.s3.us-east-2.amazonaws.com/postimages/post_two_memory_game.png',
+      published: false,
+      userId: user.id,
+      categories: {
+        create: [
+          { name: 'React' },
+          {
+            name: 'Learning'
+          }
+        ]
+      }
+    }
+  })
+  const post3 = await prisma.post.create({
+    data: {
+      title: 'Using past work to inform current workflow',
+      body: 'From the depths of graduate school',
+      postImg: 'https://blogphotosbucket.s3.us-east-2.amazonaws.com/postimages/post_three_yeast_plates.jpeg',
+      published: true,
+      userId: user.id,
+      categories: {
+        create: [
+          { name: 'Genetics' },
+          {
+            name: 'Science'
+          }
+        ]
+      }
+    }
+  })
+
+  console.log(`Database has been seeded. 🌱`)
+}
+
+seed()
+  .catch(e => {
+    console.error(e)
+    process.exit(1)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
